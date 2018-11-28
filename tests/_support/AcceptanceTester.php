29bf53e06e2fcf841f293c8b120c5ad2f99b9157
Yurii17 <37590571+Yurii17@users.noreply.github.com>
2018-11-28 15:12:34 +0200

    release v1

--- tests/_support/AcceptanceTester.php
+++ tests/_support/AcceptanceTester.php
@@ -53,104 +53,90 @@ class AcceptanceTester extends \Codeception\Actor
         $value = $I->grabTextFrom(page::$historyGrabLine);
         $I->click(page::$historyFilterTypeDrop);
         $I->click(page::$historyFilterTypeInput);                           // Входящий
-        $I->click(page::$historyFilterRefreshBtn);
         $I->wait(2);
         $I->seeElement(page::$historyFilterEmptyValue);                     // Пусте значення поки
         $I->click(page::$historyFilterTypeDrop);
         $I->click(page::$historyFilterTypeOutput);                          // Исходящий
-        $I->click(page::$historyFilterRefreshBtn);
         $I->wait(2);
         $value1 = $I->grabTextFrom(page::$historyGrabLine);                 // Грабаэться цілий рядок
-        $I->assertNotSame($value, $value1);
+        $I->assertSame($value, $value1);
         $I->click(page::$historyFilterTypeStatus);
         $value2 = $I->grabTextFrom(page::$historyFilterTypeStatusDrop[0]);  // Не подтвержден
         $I->click(page::$historyFilterTypeStatusDrop[0]);
-        $I->click(page::$historyFilterRefreshBtn);
         $I->wait(2);
         $value3 = $I->grabTextFrom(page::$historyStatusCol);
         $I->assertSame($value2, $value3);
         $I->click(page::$historyFilterTypeStatus);
         $value4 = $I->grabTextFrom(page::$historyFilterTypeStatusDrop[1]);  // Подтвержден
         $I->click(page::$historyFilterTypeStatusDrop[1]);
-        $I->click(page::$historyFilterRefreshBtn);
         $I->wait(2);
         $value5 = $I->grabTextFrom(page::$historyStatusCol);
         $I->assertSame($value4, $value5);
         $I->click(page::$historyFilterTypeStatus);
         $value6 = $I->grabTextFrom(page::$historyFilterTypeStatusDrop[2]);  // В Очереди
         $I->click(page::$historyFilterTypeStatusDrop[2]);
-        $I->click(page::$historyFilterRefreshBtn);
         $I->wait(2);
         $value7 = $I->grabTextFrom(page::$historyStatusCol);
         $I->assertSame($value6, $value7);
         $I->click(page::$historyFilterTypeStatus);
         $value8 = $I->grabTextFrom(page::$historyFilterTypeStatusDrop[3]);  // Исполнен  - пусте значення поки
         $I->click(page::$historyFilterTypeStatusDrop[3]);
-        $I->click(page::$historyFilterRefreshBtn);
         $I->wait(2);
         $value9 = $I->grabTextFrom(page::$historyFilterEmptyValue);          // Исполнен  - пусте значення поки
         $I->assertNotSame($value8, $value9);
         $I->click(page::$historyFilterTypeStatus);
         $value10 = $I->grabTextFrom(page::$historyFilterTypeStatusDrop[4]);  // Отменен
         $I->click(page::$historyFilterTypeStatusDrop[4]);
-        $I->click(page::$historyFilterRefreshBtn);
         $I->wait(2);
         $value11 = $I->grabTextFrom(page::$historyStatusCol);
         $I->assertSame($value10, $value11);
         $I->click(page::$historyFilterTypeStatus);
         $value12 = $I->grabTextFrom(page::$historyFilterTypeStatusDrop[5]);  // В процессе
         $I->click(page::$historyFilterTypeStatusDrop[5]);
-        $I->click(page::$historyFilterRefreshBtn);
         $I->wait(2);
         $value13 = $I->grabTextFrom(page::$historyFilterEmptyValue);
         $I->assertNotSame($value12, $value13);
         $I->reloadPage('/account/history');
+        $I->wait(2);
         $I->click(page::$historyFilterSystemPayDrop);
         $value14 = $I->grabTextFrom(page::$historyFilterSystemPay[0]);       // Все Системи
         $I->click(page::$historyFilterSystemPay[0]);
-        $I->click(page::$historyFilterRefreshBtn);
         $I->wait(2);
         $value15 = $I->grabTextFrom(page::$historySystemPayCol);
         $I->assertNotSame($value14, $value15);
         $I->click(page::$historyFilterSystemPayDrop);
         $value16 = $I->grabTextFrom(page::$historyFilterSystemPay[1]);       // Paxum
         $I->click(page::$historyFilterSystemPay[1]);
-        $I->click(page::$historyFilterRefreshBtn);
         $I->wait(2);
         $value17 = $I->grabTextFrom(page::$historySystemPayCol);
         $I->assertSame($value16, $value17);                                  // Paxum Paxum
         $I->click(page::$historyFilterSystemPayDrop);
         $value18 = $I->grabTextFrom(page::$historyFilterSystemPay[2]);       // Advanced Cash
         $I->click(page::$historyFilterSystemPay[2]);
-        $I->click(page::$historyFilterRefreshBtn);
         $I->wait(2);
         $value19 = $I->grabTextFrom(page::$historyFilterEmptyValue);         // Advanced Cash - Empty Result
         $I->assertNotSame($value18, $value19);
         $I->click(page::$historyFilterSystemPayDrop);
         $value20 = $I->grabTextFrom(page::$historyFilterSystemPay[3]);       // WebMoney
         $I->click(page::$historyFilterSystemPay[3]);
-        $I->click(page::$historyFilterRefreshBtn);
         $I->wait(2);
         $value21 = $I->grabTextFrom(page::$historySystemPayCol);             // WebMoney
         $I->assertSame($value20, $value21);
         $I->click(page::$historyFilterSystemPayDrop);
         $value22 = $I->grabTextFrom(page::$historyFilterSystemPay[4]);       // Capitalist
         $I->click(page::$historyFilterSystemPay[4]);
-        $I->click(page::$historyFilterRefreshBtn);
         $I->wait(2);
         $value23 = $I->grabTextFrom(page::$historyFilterEmptyValue);
         $I->assertNotSame($value22, $value23);                               // Capitalist - Empty Result
         $I->click(page::$historyFilterSystemPayDrop);
         $value24 = $I->grabTextFrom(page::$historyFilterSystemPay[5]);       // Perfect Money
         $I->click(page::$historyFilterSystemPay[5]);
-        $I->click(page::$historyFilterRefreshBtn);
         $I->wait(2);
         $value25 = $I->grabTextFrom(page::$historySystemPayCol);
         $I->assertSame($value24, $value25);                                  // Perfect Money
         $I->click(page::$historyFilterSystemPayDrop);
         $value26 = $I->grabTextFrom(page::$historyFilterSystemPay[6]);       // PayPal - PP
         $I->click(page::$historyFilterSystemPay[6]);
-        $I->click(page::$historyFilterRefreshBtn);
         $I->wait(2);
         $value27 = $I->grabTextFrom(page::$historySystemPayCol);
         $I->assertSame($value26, $value27);
