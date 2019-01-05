0eb5e17e6c5e79c0029a0ce0dd7fa9785abd12f2
Yurii17 <37590571+Yurii17@users.noreply.github.com>
2019-01-05 16:59:19 +0200

    release v1

--- tests/api/profileCest.php
+++ tests/api/profileCest.php
@@ -5,7 +5,8 @@ require_once __DIR__.'/loginCest.php';
 
 class profileCest
 {
-    public $route = ['/profile' , '/ticket', '/transfer?lang=ru'];
+    public $route = ['/profile' , '/ticket', '/transfer?lang=ru',
+        '/walletTransactionMass?lang=ru', '/transferService?lang=ru', '/deposit?lang=ru'];
 
     /**
      * @param ApiTester $I
@@ -42,69 +43,13 @@ class profileCest
     }
 
 
-    public function sendPostTicketValid(ApiTester $I)
-    {
-        $I->haveHttpHeader('Content-type', 'application/json');
-        $I->removeAuthHeader();
-        $I->sendPOST($this->route[1] ,[
-            "data" => ["access_token" =>
-                "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImI0NDk3ZmM4YjE4ODY3MjJkM2Y4YWEwY2VjMjg
-                4NThlZTI3YTZmZTdlMDQxYmY3MTlkZThkYjBhYjg5MDBmODViMzZiNTRjYjJlMzAwMzdlIn0.eyJhdWQiOiI
-                zIiwianRpIjoiYjQ0OTdmYzhiMTg4NjcyMmQzZjhhYTBjZWMyODg1OGVlMjdhNmZlN2UwNDFiZjcxOWRlOGR
-                iMGFiODkwMGY4NWIzNmI1NGNiMmUzMDAzN2UiLCJpYXQiOjE1NDQ0Mzg2NDIsIm5iZiI6MTU0NDQzODY0Miw
-                iZXhwIjoxNTc1OTc0NjQyLCJzdWIiOiIzOCIsInNjb3BlcyI6W119.gE-xwg8zNg3LhWL5TSDsDqDI-6gmlSE0
-                -K9Sa9LMDvz_p2SYCszbOf3Vg51-Bm9O-ThnqxclFoT2NC8pYBh_Adk50wTIhnK05GN-Me3gGZ1-3VnDPxHgNOH
-                _bUSm9mCDh4aIqb-FDmt8uFA9QQ517YbK7ERqzxo-SQ231qc2IKOgGOoItyVt48ocnCU7AGeTQLiJhC4d31R9Kc
-                YswijYNtV0hhVm4ak9uvhROfEkyL5Icsg1bEwU5LGbtHxnULJGk5UcRdWa2IMFkpjP3tsUsay5erTk7cEHAt0PvW
-                _r0uB3SmntcehuphyZy98zZL36vi0w_tmYnkIRmqeQLDWdyZdLdXYg-I0QUvTr4qvAd1ymBci7TDBsent3eOVBfhi
-                LdDBLD3Sw7VqG0wSJGqgxE6MrkDgnsxhtJmTKDf1myhKwNIEqC7k1r0VnLgdZhqedUpLaKbRK9SAv1evCgRbHW8vA
-                BgSN0oOg66WPNwAORq8dx7eCQQJq2CQbfQ2RtOw55dfFk2mHFxIoZd0pFLOJ1TBhzMIhZg6Jrh8R5k2ACSyQ_dLVnn
-                CURS9fSop8AMTobNnGJs81rV1c8ufyZZWpd3xH93c9GTfcouNAZB7FXBJXL_C9gXIzOAQQLG94FZS5GfY53VnTrytyd
-                4eos-ioxo-VsJhod94XE-fwrvRXopU",
-                "token_type" => "Bearer",
-                "expires_at" => "2018-12-10 10:59:02",
-                "id" => 38,
-                "email" => "yurii.lobas@gmail.com",
-                "name" => "Yurii QA",
-                "sign" => null,
-                "user_contacts" => null,
-                "user_signup_time" => "2018-10-16 11:11:14",
-                "user_status" => 2,
-                "user_config" => ["cryptoProxy_BTC" => "2N8S9KjaywUPrmjrvL97re1mpDeWkysZhUq",
-                    "cryptoProxy_LTC" => "2Mu4yAAj2eJ34pPcevwwLGRQdfGK34ZH62G",
-                    "email_notifications_disabled" => [],
-                    "ip" => ["123.123.123.1"],
-                    "session_timeout" => 900],
-                "user_memory" => ["ui_money_hide" => false],
-                "user_from" => null,
-                "user_ip" => "193.93.219.113",
-                "balance" => [["id" => 27,
-                    "wallet_id" => "JU181016909",
-                    "balance" => "8768755.45",
-                    "currency" => "USD"]],
-                "limit" => ["limit_out_daily" => 9000000,
-                    "limit_out_daily_used" => 0,
-                    "limit_out_daily_used_percent" => 0,
-                    "limit_available" => 9000000],
-                "server_time" => "2018-12-10 10:44:03",
-                "server_timezone" => "UTC",
-                "paypin_secret" => "WBUHX5JQSE2Y7XHJ",
-                "paypin_barcode_url" =>
-                    "https://chart.googleapis.com/chart?chs=200x200&chld=M|0
-                    &cht=qr&chl=otpauth%3A%2F%2Ftotp%2FjWallet%3Fsecret%3DWBUHX5JQSE2Y7XHJ"],
-                "message" => "success"
-        ]);
-        $I->seeAuthErrorMessage();
-
-    }
-
     // ApiAccountTransfer
 
     /**
      * @param ApiTester $I
      * @throws Exception
      */
-    public function JWReceiverError(ApiTester $I)
+    public function sendPostJWReceiverError(ApiTester $I)               //Transfer/JW
     {
         $I->sendPOST($this->route[2], [
             "eps_code" => "JW",
@@ -117,28 +62,39 @@ class profileCest
         $I->seeResponseCodeIs(400);
         $I->seeErrorMessage(["Неправильно введен получатель"]);
     }
-    /**
-     * @param ApiTester $I
-     * @throws Exception
-     */
-    public function JWAmountLimitError(ApiTester $I)
+
+    public function sendPostJWValueError(ApiTester $I)                  //Transfer/JW
     {
         $I->sendPOST($this->route[2], [
             "eps_code" => "JW",
             "transfer_input" => "transfer_send",
-            "transfer_receive_amount" => "10000000.00",
+            "transfer_receive_amount" => "1e+21",
             "transfer_receiver_type" => "text",
-            "transfer_receiver_value" => "JU000000000",
-            "transfer_send_amount" => "10000000"
+            "transfer_receiver_value" => "JU000000",
+            "transfer_send_amount" => "999999999999999999999"
         ]);
         $I->seeResponseCodeIs(400);
-        $I->seeErrorMessage(["Превышен лимит. Максимальная сумма отправления - 8999768.58USD"]);
+        $I->seeErrorMessage(["Введите корректную сумму"]);
     }
     /**
      * @param ApiTester $I
      * @throws Exception
      */
-    public function JWMinAmountError(ApiTester $I)
+    public function sendPostJWAmountLimitError(ApiTester $I)             //Transfer/JW
+    {
+        $I->sendPOST($this->route[2], [
+            "eps_code" => "JW",
+            "transfer_input" => "transfer_send",
+            "transfer_receive_amount" => "8999999.00",
+            "transfer_receiver_type" => "text",
+            "transfer_receiver_value" => "JU000000000",
+            "transfer_send_amount" => "8999999"
+        ]);
+        $I->seeResponseCodeIs(400);
+        $I->seeErrorMessage(["Превышен лимит. Максимальная сумма отправления - 8999986USD"]);
+    }
+
+    public function sendPostJWMinAmountError(ApiTester $I)                //Transfer/JW
     {
         $I->sendPOST($this->route[2], [
             "eps_code" => "JW",
@@ -151,11 +107,8 @@ class profileCest
         $I->seeResponseCodeIs(400);
         $I->seeErrorMessage(["Минимальная сумма отправления - $2"]);
     }
-    /**
-     * @param ApiTester $I
-     * @throws Exception
-     */
-    public function JWSendAmountError(ApiTester $I)
+
+    public function sendPostJWSendAmountError(ApiTester $I)                //Transfer/JW
     {
         $I->sendPOST($this->route[2], [
             "eps_code" => "JW",
@@ -169,11 +122,8 @@ class profileCest
         $I->seeErrorMessage(["transfer_send_amount" =>
         ["Поле 'Сумма отправления' должно быть числом."]]);
     }
-    /**
-     * @param ApiTester $I
-     * @throws Exception
-     */
-    public function JWTextError(ApiTester $I)
+
+    public function sendPostJWTextError(ApiTester $I)                        //Transfer/JW
     {
         $I->sendPOST($this->route[2], [
             "eps_code" => "JW",
@@ -191,11 +141,191 @@ class profileCest
         ["Количество символов в поле 'Предмет перевода' не может превышать 200."]]);
     }
 
+    public function sendPostJWNullError(ApiTester $I)                            //Transfer/JW
+    {
+        $I->sendPOST($this->route[2], [
+            "eps_code" => "JW",
+            "transfer_input" => "transfer_send",
+            "transfer_receive_amount" => null,
+            "transfer_receiver_type" => "text",
+            "transfer_receiver_value" => "JU000000000",
+            "transfer_send_amount" => null
+        ]);
+        $I->seeResponseCodeIs(400);
+        $I->seeErrorMessage([]);
+    }
 
+    public function sendPostBTCFakeError(ApiTester $I)                             //Transfer/BTC
+    {
+        $I->sendPOST($this->route[2], [
+            "eps_code" => "BTC",
+            "transfer_input" => "transfer_send",
+            "transfer_receive_amount" => "0.051035",
+            "transfer_receiver" => fake::create()->randomDigit,
+            "transfer_send_amount" => "333"
+        ]);
+        $I->seeResponseCodeIs(400);
+        $I->seeErrorMessage(["Укажите корректный кошелек получателя"]);
+    }
 
+    public function sendPostBTCMinAmountError(ApiTester $I)                         //Transfer/BTC
+    {
+        $I->sendPOST($this->route[2], [
+            "eps_code" => "BTC",
+            "transfer_input" => "transfer_send",
+            "transfer_receive_amount" => "0.001048",
+            "transfer_receiver" => "222222222222222222222222",
+            "transfer_send_amount" => "10"
+        ]);
+        $I->seeResponseCodeIs(400);
+        $I->seeErrorMessage(["Минимальная сумма к получению - 0.01 BTC"]);
+    }
 
+    public function sendPostBTCMaxAmountError(ApiTester $I)                         //Transfer/BTC
+    {
+        $I->sendPOST($this->route[2], [
+            "eps_code" => "BTC",
+            "transfer_input" => "transfer_send",
+            "transfer_receive_amount" => "1.361",
+            "transfer_receiver" => "222222222222222222222222",
+            "transfer_send_amount" => "8797.49"
+        ]);
+        $I->seeResponseCodeIs(400);
+        $I->seeErrorMessage(["Максимальная сумма к получению - 1.36 BTC"]);
+    }
 
+    public function sendPostBTCOK(ApiTester $I)                                      //Transfer/BTC
+    {
+        $I->sendPOST($this->route[2], [
+            "eps_code" => "BTC",
+            "transfer_input" => "transfer_send",
+            "transfer_receive_amount" => "0.01",
+            "transfer_receiver" => "222222222222222222222222",
+            "transfer_send_amount" => "67.85"
+        ]);
+        $I->seeResponseCodeIs(201);
+    }
 
+    public function sendPostPXMinTwoDeparturesError(ApiTester $I)                    // Mass-transfer
+    {
+        $I->sendPOST($this->route[3], [
+            "conf_pp_com_type" => "payee",
+            "conf_subject" => null,
+            "eps" => "PX",
+            "receiver" => fake::create()->safeEmail,
+            "receiver_type" => "email",
+            "row_index" => 0,
+            "transfer_receive" => "5.00",
+            "transfer_send" => "10"
+            ]);
+        $I->seeResponseCodeIs(400);
+        $I->seeErrorMessage(["Укажите минимум 2 отправления"]);
+    }
+
+    public function sendPostPXServiceAmountLimitError(ApiTester $I)                  // Service-payment
+    {
+        $I->sendPOST($this->route[4], [
+            "eps_code" => "PX",
+            "payee_type" => "service",
+            "service_info" => "",
+            "service_url" => "https://dev3.jwallet.cc",
+            "transfer_input" => "send",
+            "transfer_receive_amount" => "98499996.00",
+            "transfer_receive_currency" => "USD",
+            "transfer_send_amount" => "100000000",
+            "transfer_send_currency" => "USD"
+        ]);
+        $I->seeResponseCodeIs(400);
+        $I->seeErrorMessage(["Превышен лимит. Максимальная сумма отправления - $9000000.00"]);
+    }
+
+    public function sendPostPXUrlError(ApiTester $I)                                  // Service-payment
+    {
+        $I->sendPOST($this->route[4], [
+            "eps_code" => "PX",
+            "payee_type" => "service",
+            "service_info" => "",
+            "service_url" => "https://dev3.jwallet.ccLorem ipsum dolor ",
+            "transfer_input" => "send",
+            "transfer_receive_amount" => "98499996.00",
+            "transfer_receive_currency" => "USD",
+            "transfer_send_amount" => "100000000",
+            "transfer_send_currency" => "USD"
+        ]);
+        $I->seeResponseCodeIs(400);
+        $I->seeErrorMessage(["service_url" => ["Поле 'Сылка на оплату' имеет ошибочный формат."]]);
+    }
+
+    public function sendPostPXOK(ApiTester $I)                                        // Service-payment
+    {
+        $I->sendPOST($this->route[4], [
+            "eps_code" => "PX",
+            "payee_type" => "service",
+            "service_info" => "",
+            "service_url" => "https://dev3.jwallet.cc ",
+            "transfer_input" => "send",
+            "transfer_receive_amount" => "0.92",
+            "transfer_receive_currency" => "USD",
+            "transfer_send_amount" => "5",
+            "transfer_send_currency" => "USD"
+        ]);
+        $I->seeResponseCodeIs(201);
+    }
+
+    public function sendPostBTCMaxTransferError(ApiTester $I)                           // Deposit/BTC
+    {
+        $I->sendPOST($this->route[5], [
+            "eps_code" => "BTC",
+            "request_type" => "crypto",
+            "transfer_input" => "transfer_send",
+            "transfer_receive" => "627483.64",
+            "transfer_send" => "100"
+        ]);
+        $I->seeResponseCodeIs(400);
+        $I->seeErrorMessage(["Максимальная сумма отправления - 1.13 BTC"]);
+    }
+
+    public function sendPostBTCTransferSendError(ApiTester $I)                      // Deposit/BTC
+    {
+        $I->sendPOST($this->route[5], [
+            "eps_code" => "BTC",
+            "request_type" => "crypto",
+            "transfer_input" => "transfer_send",
+            "transfer_receive" => "6274.84",
+            "transfer_send" => "1&$*($@#"
+        ]);
+        $I->seeResponseCodeIs(400);
+        $I->seeErrorMessage(["transfer_send" => ["Поле 'Сумма отправления' должно быть числом."]]);
+    }
+
+    public function sendPostWEXFakeCodeError(ApiTester $I)                             // Deposit/WEX
+    {
+        $I->sendPOST($this->route[5], [
+            "eps_code" => "WEX",
+            "money_code" => fake::create()->text
+        ]);
+        $I->seeResponseCodeIs(400);
+        $I->seeErrorMessage(["Поддерживаются только USD коды"]);
+    }
+
+    public function sendPostWEXNullCodeError(ApiTester $I)                             // Deposit/WEX
+    {
+        $I->sendPOST($this->route[5], [
+            "eps_code" => "WEX",
+            "money_code" => null
+        ]);
+        $I->seeResponseCodeIs(400);
+        $I->seeErrorMessage([]);
+    }
+
+    public function sendPostWEXCodeOK(ApiTester $I)                                     // Deposit/WEX
+    {
+        $I->sendPOST($this->route[5], [
+            "eps_code" => "WEX",
+            "money_code" => "WEXUSD0000000000000000000000000000000000000000"
+        ]);
+        $I->seeResponseCodeIs(200);
+    }
 
 
 
